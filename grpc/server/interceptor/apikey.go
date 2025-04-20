9cfe871fd315fd67f17b9fd6daa0cccd684f808e
Nicholas Terrell <nicholas@terrell.com.au>
2025-04-20 23:21:46 +1000

    exclude healthcheck from apikey

--- grpc/server/interceptor/apikey.go
+++ grpc/server/interceptor/apikey.go
@@ -10,6 +10,9 @@ import (
 
 func APIKey(apiKey string) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		if info.FullMethod == "/grpc.health.v1.Health/Check" {
+			return handler(ctx, req)
+		}
 		if apiKey == "" || apiKey != getMetadataValue(ctx, MetadataAPIKeyKey) {
 			return nil, status.Error(codes.Unauthenticated, "Invalid API key")
 		}
